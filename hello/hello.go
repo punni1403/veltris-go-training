10b3442daaded9b7041deeaeca6059d4fcf76581
Puneet Kumar <puneet@nbos.vinisoft.com>
2024-04-26 12:30:39 +0530

    training material day 2

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package hello
+
+import "fmt"
+
+func smallCaefunc() {
+	fmt.Print("something")
+} // private function -
+func LargeCasefunc() {
+	fmt.Println("somemorther thing")
+} // public function
